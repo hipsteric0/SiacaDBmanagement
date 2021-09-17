d33b291a18ab983d1bcc08e3ba6d65eadf1c212a
hipsteric0 <aleromerosurf@gmail.com>
2021-09-17 15:49:45 -0400

    prueba de conexion entre la api y base de datos de prueba

--- SiacaCSAPI/SiacaCSAPI/Controllers/DBManagementController.cs
+++ SiacaCSAPI/SiacaCSAPI/Controllers/DBManagementController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,8 +17,56 @@ namespace SiacaCSAPI.Controllers
 		[HttpGet]
 		public IActionResult Get()
 		{
-			return Ok("wenas");
+			try
+			{
+				SqlConnection con = new SqlConnection();
+				con.ConnectionString = "Data Source=PC-DE-ALEJANDRO;Initial Catalog=Prueba;Integrated Security=True";
+				int Id = 2;
+
+				var query = "SELECT id,nombre,apellido,cedula FROM TablaDePrueba WHERE id = @ID;";
+				String str = "";
+
+				SqlCommand cmd = new SqlCommand(query, con);
+
+				cmd.Parameters.Add(new SqlParameter("@ID", Id));
+				DataTable dt = new DataTable();
+				SqlDataAdapter da = new SqlDataAdapter(cmd);
+				con.Open();
+				//datareader quizas es una mejor forma de sacar valores
+				da.Fill(dt);
+
+				foreach (DataRow row in dt.Rows)
+				{
+
+					String Name = row["Nombre"].ToString();
+					String FamilyName = row["Apellido"].ToString();
+					String ID = row["Cedula"].ToString();
+					str = Name + " " + FamilyName + " " + ID;
+				}
+
+				// (using) is BS
+				//make sure to check for null values when retrieving the data
+
+
+
+
+				//MessageBox.Show(str);
+				Console.WriteLine(str);
+				return Ok(str);
+			}
+			catch(Exception e)
+			{
+				return BadRequest(e);
+			}
+			finally
+			{
+
+			}
+
+			return Ok("no data");
+
 		}
+
 
 		[HttpPost]
 		public IActionResult Post(JObject payload)
@@ -24,4 +74,5 @@ namespace SiacaCSAPI.Controllers
 			return Ok(payload);
 		}
 	}
+	
 }
---
+++ SiacaCSAPI/SiacaCSAPI/Entities/TablaDePrueba.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SiacaCSAPI.Entities
+{
+	public class TablaDePrueba
+	{
+		public double id;
+		public string nombre;
+		public string apellido;
+		public double cedula;
+	}
+}
