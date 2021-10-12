166020633a19727aa7a4da1b7be27a93e8d565a1
hipsteric0 <aleromerosurf@gmail.com>
2021-10-12 15:24:46 -0400

    sleep funcional. se debe deshabilitar boton

--- SiacaCSSQLServerDB/SiacaCSSQLServerDB/Form1.cs
+++ SiacaCSSQLServerDB/SiacaCSSQLServerDB/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -22,72 +23,78 @@ namespace SiacaCSSQLServerDB
 			InitializeComponent();
 		}
 
-		private void button1_Click(object sender, EventArgs e)
+		private async void button1_Click(object sender, EventArgs e)
 		{
 			LoggerSQLServer logger = new LoggerSQLServer();
-			
-			
 
-			if (oldCantidadDeFacturas == 0)
+
+			while (true)
 			{
-				//oldCantidadDeFacturas = cambios.SelectCantidadDeFacturas();  // Esto es si el programa funcionara con la cantidad de registros en saFacturaCompra
-				oldCantidadDeFacturas = cambios.SelectCantidadDeNotasRecepcionCompra();    // Esto es si el programa funcionara con la cantidad de registros en saNotaRecepcionCompraReng
-				oldCantidadDeFacturas = 643; //BORRAR, SOLO PARA PRUEBAS error ñs 644, los acentos tambien dan error 649
-			}
-
-			//int newCantidadDeFacturas = cambios.SelectCantidadDeFacturas();  // Esto es si el programa funcionara con la cantidad de registros en saFacturaCompra			
-			int newCantidadDeFacturas = cambios.SelectCantidadDeNotasRecepcionCompra();    // Esto es si el programa funcionara con la cantidad de registros en saNotaRecepcionCompraReng
-
-			if (oldCantidadDeFacturas== newCantidadDeFacturas)
-			{
-				// no hay facturas nuevas, 
-				int sleepQuantity = 5; // se debera dormir 5 minutos si no hay cambiuos nuevos
-			}
-			else if ((newCantidadDeFacturas == -1) || (oldCantidadDeFacturas== -1) )// en este caso no hay conexion a la bd de sqlserver local
-			{
-				//escribir en el LOG que no hay conexion a la BD 
-				logger.BDProfitCaidaLogger();
-				int sleepQuantity = 5; // se debera dormir 5 minutos si no hay conexion
-			}
-			else
-			{
-				//si hay cambios, se deben procesar
-				int cantidadCambios = newCantidadDeFacturas - oldCantidadDeFacturas;
-				//int cambioInsertado = cambios.BuscarCambioEnProfit(cantidadCambios);// Esto es si el programa funcionara con la cantidad de registros en saFacturaCompra
-				int cambioInsertado = cambios.BuscarCambioEnProfitNotaCompra(cantidadCambios);// Esto es si el programa funcionara con la cantidad de registros en saNotaRecepcionCompraReng
+				
 
 
-				if (cambioInsertado == 1) //si el cambio se inserto, se escribe en el log
+				if (oldCantidadDeFacturas == 0)
 				{
-					//escribir en el log cambio correctamente insertado y anadir uno a la cantidad de facturas viejas
-					oldCantidadDeFacturas++;
-					//si quedan cambios sleep de 20 segundos, sino sleep de 5 mins
+					//oldCantidadDeFacturas = cambios.SelectCantidadDeFacturas();  // Esto es si el programa funcionara con la cantidad de registros en saFacturaCompra
+					oldCantidadDeFacturas = cambios.SelectCantidadDeNotasRecepcionCompra();    // Esto es si el programa funcionara con la cantidad de registros en saNotaRecepcionCompraReng
+					oldCantidadDeFacturas = 643; //BORRAR, SOLO PARA PRUEBAS error ñs 644, los acentos tambien dan error 649
 				}
-				else //el cambio no se envio, se volvera a intentar
+
+				//int newCantidadDeFacturas = cambios.SelectCantidadDeFacturas();  // Esto es si el programa funcionara con la cantidad de registros en saFacturaCompra			
+				int newCantidadDeFacturas = cambios.SelectCantidadDeNotasRecepcionCompra();    // Esto es si el programa funcionara con la cantidad de registros en saNotaRecepcionCompraReng
+
+				if (oldCantidadDeFacturas == newCantidadDeFacturas)
 				{
-					//sleep de un minuto
+					// no hay facturas nuevas, 
+					int sleepQuantity = 5; // se debera dormir 5 minutos si no hay cambiuos nuevos
 				}
+				else if ((newCantidadDeFacturas == -1) || (oldCantidadDeFacturas == -1))// en este caso no hay conexion a la bd de sqlserver local
+				{
+					//escribir en el LOG que no hay conexion a la BD 
+					logger.BDProfitCaidaLogger();
+					int sleepQuantity = 5; // se debera dormir 5 minutos si no hay conexion
+				}
+				else
+				{
+					//si hay cambios, se deben procesar
+					int cantidadCambios = newCantidadDeFacturas - oldCantidadDeFacturas;
+					//int cambioInsertado = cambios.BuscarCambioEnProfit(cantidadCambios);// Esto es si el programa funcionara con la cantidad de registros en saFacturaCompra
+					int cambioInsertado = cambios.BuscarCambioEnProfitNotaCompra(cantidadCambios);// Esto es si el programa funcionara con la cantidad de registros en saNotaRecepcionCompraReng
+
+
+					if (cambioInsertado == 1) //si el cambio se inserto, se escribe en el log
+					{
+						//escribir en el log cambio correctamente insertado y anadir uno a la cantidad de facturas viejas
+						oldCantidadDeFacturas++;
+						//si quedan cambios sleep de 20 segundos, sino sleep de 5 mins
+					}
+					else //el cambio no se envio, se volvera a intentar
+					{
+						//sleep de un minuto
+					}
+				}
+
+				textBox1.Text = logger.GetAllFile();
+
+				//ahora se verificara si hay consumos
+
+				Consumos Newconsumos = new Consumos();
+
+				int resultado = Newconsumos.Compare(Oldconsumos); //intentara buscar consumos, y si los hay, los enviara a la api
+
+				if (resultado != -1)
+				{
+					Oldconsumos = Newconsumos; // si la api no esta activa no se actualiza la tabla, cuando este activa se actualizara
+				}
+
+
+
+				textBox1.Text = logger.GetAllFile();
+
+				//sleep(sleepQuantity)
+				await Task.Delay(300000); //tiempo en milisegundos
+				//aca se duerme la cantidad que se determino
 			}
-
-			textBox1.Text = logger.GetAllFile();
-
-			//ahora se verificara si hay consumos
-
-			Consumos Newconsumos = new Consumos();
-			
-			int resultado =Newconsumos.Compare(Oldconsumos); //intentara buscar consumos, y si los hay, los enviara a la api
-			
-			if (resultado != -1)
-			{
-				Oldconsumos = Newconsumos; // si la api no esta activa no se actualiza la tabla, cuando este activa se actualizara
-			}
-			
-
-
-			textBox1.Text = logger.GetAllFile();
-
-			//sleep(sleepQuantity)
-			//aca se duerme la cantidad que se determino
 
 		}
 
--- SiacaCSSQLServerDB/SiacaCSSQLServerDB/Program.cs
+++ SiacaCSSQLServerDB/SiacaCSSQLServerDB/Program.cs
@@ -17,6 +17,7 @@ namespace SiacaCSSQLServerDB
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new Form1());
+			
 		}
 	}
 }
